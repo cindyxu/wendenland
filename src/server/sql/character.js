30c6787cfc13778123cb0b6955fa1bd032b7bf13
Cindy <cindy@cindys-macbook.fios-router.home>
2016-10-29 11:08:37 -0400

    comment out character sql logic, unused for now

--- src/server/sql/character.js
+++ src/server/sql/character.js
@@ -10,21 +10,23 @@ var characterSql = {
 };
 
 characterSql.setup = function(db) {
-    var promise = BPromise.resolve()
-        // create
-        .then(DbUtil.prepare(characterSchema.insert(
-            characterSchema.name.value(""),
-            characterSchema.user_id.value("")), db))
-        .then(function(st) {
-            characterSql._createStatement = st;
-        });
+    // var promise = BPromise.resolve()
+    //     // create
+    //     .then(DbUtil.prepare(characterSchema.insert(
+    //         characterSchema.name.value(""),
+    //         characterSchema.user_id.value("")), db))
+    //     .then(function(st) {
+    //         characterSql._createStatement = st;
+    //     });
 
-    return promise;
+    // return promise;
+    throw "Not implemented!";
 };
 
 characterSql.create = function(name, userId) {
-    return characterSql._createStatement.runAsync([name, userId])
-        .then(function() { return this.lastID });
+    // return characterSql._createStatement.runAsync([name, userId])
+    //     .then(function() { return this.lastID });
+    throw "Not implemented!";
 };
 
 module.exports = characterSql;
