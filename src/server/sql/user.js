d1d41695be9285ed8ba894d0e294692fd149905e
Cindy <cindy@cindys-macbook.fios-router.home>
2016-10-29 10:54:40 -0400

    comment out the sql, unused for now

--- src/server/sql/user.js
+++ src/server/sql/user.js
@@ -11,33 +11,36 @@ var userSql = {
 };
 
 userSql.setup = function(db) {
-    var promise = BPromise.resolve()
+    // var promise = BPromise.resolve()
 
-        // create
-        .then(DbUtil.prepare(userSchema.insert(
-            userSchema.username.value(""),
-            userSchema.password_hash.value("")), db))
-        .then(function(st) {
-            userSql._createStatement = st;
-        })
+    //     // create
+    //     .then(DbUtil.prepare(userSchema.insert(
+    //         userSchema.username.value(""),
+    //         userSchema.password_hash.value("")), db))
+    //     .then(function(st) {
+    //         userSql._createStatement = st;
+    //     })
 
-        // find by username
-        .then(DbUtil.prepare(userSchema.select()
-            .where(userSchema.username.equals("")), db))
-        .then(function(st) {
-            userSql._findByUsernameStatement = st;
-        });
+    //     // find by username
+    //     .then(DbUtil.prepare(userSchema.select()
+    //         .where(userSchema.username.equals("")), db))
+    //     .then(function(st) {
+    //         userSql._findByUsernameStatement = st;
+    //     });
     
-    return promise;
+    // return promise;
+    throw "Not implemented!";
 };
 
 userSql.create = function(username, passwordHash) {
-    return userSql._createStatement.runAsync([username, passwordHash])
-        .then(function() { return this.lastID });
+    // return userSql._createStatement.runAsync([username, passwordHash])
+    //     .then(function() { return this.lastID });
+    throw "Not implemented!";
 };
 
 userSql.findByUsername = function(username) {
-    return userSql._findByUsernameStatement.getAsync([username]);
+    // return userSql._findByUsernameStatement.getAsync([username]);
+    throw "Not implemented!";
 };
 
 module.exports = userSql;
